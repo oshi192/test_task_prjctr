2ddf63561d7af006592591c02292b4b7da117b86
Yurii Kosakivskyi <yurii.kosakivskyi@hpe.com>
2022-07-30 13:43:33 +0200

    Base commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from fastapi import FastAPI
+
+import pickle
+maxlen=190
+
+with open('./test_nlp/data/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+# Restore the weights
+
+model = tf.keras.models.Sequential([
+        tf.keras.layers.Embedding(10000, 16, input_length=maxlen),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20, return_sequences=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20)),
+        tf.keras.layers.Dense(190, activation='relu'),
+        tf.keras.layers.Dense(1, activation='linear')
+])
+model.load_weights('./test_nlp/data/test_model')
+
+def make_predict(text: str):
+        sequence = tokenizer.texts_to_sequences([text])
+        padded = tf.keras.preprocessing.sequence.pad_sequences(sequence, truncating='post', padding='post', maxlen=maxlen)
+        p = model.predict(padded)[0]
+        return p[0]
+
+app = FastAPI()
+
+@app.get("/test-nlp")
+def read_classification(text: str):
+        return str(make_predict(text))
---
+++ test_nlp/train_model.py
@@ -0,0 +1,73 @@
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# import tensorflow as tf
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import pickle
+# #import seaborn as sns
+#
+# maxlen = 190
+#
+# def show_history(h):
+#     epochs_trained = len(h.history['loss'])
+#     plt.plot(range(0, epochs_trained), h.history.get('loss'), label='Training')
+#     plt.plot(range(0, epochs_trained), h.history.get('val_loss'), label='Validation')
+#     plt.xlabel('Epochs')
+#     plt.ylabel('Loss')
+#     plt.legend()
+#     plt.show()
+#
+# test=pd.read_csv('test.csv', index_col=0)
+# train=pd.read_csv('train.csv', index_col=0)
+# # train
+#
+# tokenizer= Tokenizer(num_words=10000, oov_token="<OOV>")
+# tokenizer.fit_on_texts(train['excerpt'])
+#
+# lengths = [len(t.split(' ')) for t in train['excerpt']]
+# plt.hist(lengths, bins = len(set(lengths)))
+#
+# def get_sequences(tokenizer, text):
+#   sequences = tokenizer.texts_to_sequences(text)
+#   padded = pad_sequences(sequences, truncating='post', padding='post', maxlen=maxlen)
+#   return padded
+#
+# padded_train_seq = get_sequences(tokenizer, train['excerpt'])
+#
+# # saving
+# with open('./data/tokenizer.pickle', 'wb') as handle:
+#     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+# # loading
+# # with open('tokenizer.pickle', 'rb') as handle:
+# #     tokenizer = pickle.load(handle)
+#
+# model = tf.keras.models.Sequential([
+#         tf.keras.layers.Embedding(10000, 16, input_length=maxlen),
+#         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20, return_sequences=True)),
+#         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20)),
+#         tf.keras.layers.Dense(190, activation='relu'),
+#         tf.keras.layers.Dense(1, activation='linear')
+# ])
+# model.compile(
+#     loss='mean_squared_error',
+#     optimizer='adam',
+#     metrics=['accuracy']
+# )
+# model.summary()
+#
+# x_train = padded_train_seq
+# y_train = train['target']
+#
+# history = model.fit(
+#     x_train, y_train,
+#     shuffle=True,
+#     validation_split=0.2,
+#     epochs=20,
+#     verbose=1
+# )
+#
+# show_history(history)
+# model.save_weights('./data/test_model')
+#
